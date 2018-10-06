25e6bccc58f8873d0ee378261cd2053d38865071
lucius <lucianoporta@outlook.com>
2018-10-06 15:28:46 -0300

    Agregue sangrias a el codigo

--- functions.cpp
+++ functions.cpp
@@ -156,54 +156,49 @@ void mostrarListaRecur(tabla l){
 
 tipoRet eliminoTabla(tabla *tabl,string nombre){
 
-//if tiene columnas pasar a eliminar las col primero
-tabla tablon;
-tablon=*tabl;
-if (!esVaciaTabla(tablon)){
-tablon=eliminarTabla(tablon,nombre);
-
-*tabl=tablon;
-return ok;
-}else{
-return error;
-}
+    //if tiene columnas pasar a eliminar las col primero
+    tabla tablon;
+    tablon=*tabl;
+    if (!esVacia(tablon)){
+        tablon=eliminarTabla(tablon,nombre);
+        *tabl=tablon;
+        return ok;
+    }else{
+        return error;
+    }
 }
 
 tabla eliminarTabla(tabla tabl, string name){
-//0 a null 1 a 0
-tabla BorrarTabl;
-tabla anterior;
+    //0 a null 1 a 0
+    tabla BorrarTabl;
+    tabla anterior;
 
-BorrarTabl = new _tabla;
-anterior = new _tabla;
+    BorrarTabl = new _tabla;
+    anterior = new _tabla;
 
-anterior=NULL;
-BorrarTabl=tabl; //apunta al inicio de la lista
+    anterior=NULL;
+    BorrarTabl=tabl; //apunta al inicio de la lista
 
     while((BorrarTabl!=NULL)&&(BorrarTabl->nombre!=name)){ //mientras que BorrarTabl no sea null y no sea igual al nombre desdeado
-    anterior=BorrarTabl; //anterior se iguala a BorrarTabl
+        anterior=BorrarTabl; //anterior se iguala a BorrarTabl
 
-    BorrarTabl=BorrarTabl->ptrtabla; //Borrartabl va al siguiente nodo 'Tabla'
-
-
-
+        BorrarTabl=BorrarTabl->ptrtabla; //Borrartabl va al siguiente nodo 'Tabla'
     }
 
     //Casos cuando salga del while
-
     if(BorrarTabl==NULL){  // significa que recorrio toda la lista y cuando entro al while al final BorrarTabl es igual a null/ apunta al putero sig del ultimo nodo entonces NULL
-    cout << "El elemento no existe";
+        cout << "El elemento no existe";
 
-    }
-    else if(anterior==NULL){ // Si entra aca significa que no entro al while porque el elemento que buscamos es el primero en la lista, entonces  tabl apunta al sig nodo y deleteamos el primer nodo que esta en BorrarTabl.
-    tabl = tabl->ptrtabla;
-    delete BorrarTabl;
-    }
-    else{ // El elemento esta en la lista pero no es el primero
+    } else if(anterior==NULL) { // Si entra aca significa que no entro al while porque el elemento que buscamos es el primero en la lista, entonces  tabl apunta al sig nodo y deleteamos el primer nodo que esta en BorrarTabl.
+        tabl = tabl->ptrtabla;
+        delete BorrarTabl;
+
+    } else { // El elemento esta en la lista pero no es el primero
         anterior->ptrtabla=BorrarTabl->ptrtabla;  // hacemos que el que estaba atras apunte al mismo que estaba apuntando borrartabl 'el siguiente del actual'
         delete BorrarTabl; // borramos borrartabl
+
     }
 
-return tabl;
+    return tabl;
 }
 

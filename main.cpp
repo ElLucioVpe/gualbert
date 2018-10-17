28c17b562bc77701a069b6a8f429b60410e84f3c
WhySoBizarre <pagolaguillen@hotmail.com>
2018-10-16 23:15:24 -0300

    Insertar tupla. verifica num de datos  y cols
    Fixeado tambien mostrar filas,
    Fixeadas otras peque;as weas, que comente por discordio asi que no la pongo aca xD LOQUENDO

--- main.cpp
+++ main.cpp
@@ -9,25 +9,26 @@ int main()
     muestroR(insertoTabla(&tablas,"clientes"));
     muestroR(insertoTabla(&tablas,"personas"));
     muestroR(insertoTabla(&tablas,"mascotas"));
+    muestroR(insertoTabla(&tablas,"mascotas"));
 
     muestroR(insertoColumna(&tablas, "personas", "ci"));
     muestroR(insertoColumna(&tablas, "personas", "nombre"));
     muestroR(insertoColumna(&tablas, "personas", "apellido"));
         //muestroR(insertoColumna(&tablas, "personas", "correo"));
 
-    //muestroR(insertoDato(&tablas,"personas", "1:Martin:Perdomo"));
     muestroR(insertoDato(&tablas,"personas", "1:Martin:Perdomo"));
+    muestroR(insertoDato(&tablas,"personas", "4:Martin:Perdomo"));
     muestroR(insertoDato(&tablas,"personas", "4:Diego:Olivera"));
-    muestroR(insertoDato(&tablas,"personas", "7:Lautaro:KASjkdsjk"));
-    muestroR(insertoDato(&tablas,"personas", "4:Nicolas:Varela"));
+   // muestroR(insertoDato(&tablas,"personas", "7:Lautaro:KASjkdsjk"));
+   // muestroR(insertoDato(&tablas,"personas", "4:Nicolas:Varela"));
 
-    muestroR(actualizoDatos(&tablas, "personas", "ci", "1", "ci", "69"));
+   // muestroR(actualizoDatos(&tablas, "personas", "ci", "1", "ci", "69"));
     //muestroR(eliminoTabla(&tablas, "personas"));
   //  muestroR(insertoDato(&tablas,"personas", "8:SOldado:KASjkdsjk"));
   //  muestroR(insertoDato(&tablas,"personas", "9:Zi:Rica"));
  //   muestroR(insertoDato(&tablas,"personas", "710:Robert:ole"));
    // muestroR(insertoDato(&tablas,"personas", "8:Luis:OJILI"));
-
+ // muestroR(insertoColumna(&tablas, "personas", "mascotas"));
 
     //muestroR(eliminoDato(&tablas, "personas","ci","69"));
     //muestroR(eliminoDato(&tablas, "personas","ci","4"));
